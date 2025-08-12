8b11aed0669db6998c4317bf2d2bf9abede885ae
adevster8 <adevster8@gmail.com>
2025-08-11 20:58:55 -0700

    Chore: remove deprecated expo-router/babel and add alias

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ["babel-preset-expo"], // includes expo-router in SDK 50
     plugins: [
-      // ...other plugins
-      "react-native-reanimated/plugin", // keep this LAST
-    ],
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: { "@": "./app" },
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
+        }
+      ],
+      "react-native-reanimated/plugin" // keep LAST
+    ]
   };
 };
